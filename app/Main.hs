65ddaf19112e693a430c69201ad3a3e97822835e
Alexa DeWit <alexa.dewit@gmail.com>
2017-02-03 19:03:46 +0100

    Initial setup. Able to now communicate a meaningless request.

---
+++ app/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Servant
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Control.Monad.Except
+
+--Local Libs
+import Lib
+import GameSessions
+
+server :: Server SessionTokenApi
+server = game_session
+  where game_session :: LoginRequest -> Handler SessionToken
+        game_session  req = liftIO $ tokenFromRequest req
+
+gameSessionsAPI :: Proxy SessionTokenApi
+gameSessionsAPI = Proxy
+
+app1 :: Application
+app1 = serve gameSessionsAPI server
+
+main :: IO ()
+main = run 8080 app1

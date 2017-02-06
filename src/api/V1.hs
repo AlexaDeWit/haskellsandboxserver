338d4668d2e4ca2ed01ac5f7342ab342f28a7356
Alexa DeWit <alexa.dewit@gmail.com>
2017-02-06 04:10:07 +0100

    Ordningen måste finns

--- src/api/V1.hs
+++ src/api/V1.hs
@@ -29,8 +29,18 @@ appToServer cfg = enter (convertApp cfg) tokenServer
 convertApp :: Config -> App :~> ExceptT ServantErr IO
 convertApp cfg = Nat (flip runReaderT cfg . runApp)
 
+
+{-
+  The public API endpoints are not "actually" public. They are guarded by a User's
+  session token, and expose data personal to that User
+
+  The private API endpoints by contrast are accessed through client application
+  credentials and can be used to access data on any user. This api will only
+  ever be exposed to trusted first party applications. If I ever want to expose
+  some data to 3rd party clients, OpenID proper will be introduced.
+-}
 type APIV1 = "api" :> "v1" :>
-  SessionTokenApi
+  "public" :> "sessions" :> SessionTokenApi
 
 v1ServerAPI :: Proxy APIV1
 v1ServerAPI = Proxy
